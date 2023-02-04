0de91f35032599fe32c908cce01e8e40b472c86f
kouhei-r-g <103445080+kouhei-r-g@users.noreply.github.com>
2023-02-04 15:46:50 +0900

    条件分岐・演算子

---
+++ JavaScript/1-3/main.js
@@ -0,0 +1,23 @@
+// 変数aは10ではない
+let a
+a !== 10
+// 変数bは10以上20未満、且つ偶数である。
+let b
+b >= 10 && b < 20 && b % 2 === 0
+
+let x = 10;
+
+if (x >= 10 && x <= 20) {
+    console.log("成功です")
+}else{
+    console.log("失敗です")
+}
+
+let y = 0;
+if (y % 2 === 0) {
+    console.log("偶数です")
+}
+
+if (y % 2 !== 0) {
+    console.log("奇数です")
+}
