6aab2da8e66004526dfbe17f92a083759bf0f932
kouhei-r-g <103445080+kouhei-r-g@users.noreply.github.com>
2023-02-05 20:33:51 +0900

    1-6

---
+++ JavaScript/1-6/main.js
@@ -0,0 +1,16 @@
+let scores = [10, 15, 20, 25];
+
+for (let i = 0; i < scores.length; i++) {
+  if (scores[i] % 2 === 0) {
+    console.log(scores[i] + "は偶数です");
+  }
+}
+
+
+let carobject = {
+    gass: 20.5,
+    num:1234
+};
+
+console.log("ガソリンは" + carobject.gass + "です");
+console.log("ナンバーは" + carobject.num + "です");
