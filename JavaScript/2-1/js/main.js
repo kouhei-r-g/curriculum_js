83c22f36e93d319bda673fc5cf4c2b8324bb79c0
kouhei-r-g <103445080+kouhei-r-g@users.noreply.github.com>
2023-02-06 01:32:29 +0900

    2-1

---
+++ JavaScript/2-1/js/main.js
@@ -0,0 +1,10 @@
+document.body.textContent = 'hello';
+
+document.title = 'JavaScript2-1';
+
+//windowオブジェクトの中身をconsole.logで確認してみます。
+console.log(window);
+
+let url = window.location.href;
+
+console.log(url);
