99452cc9b313807179e08964040229af8eedfd45
kouhei-r-g <103445080+kouhei-r-g@users.noreply.github.com>
2023-02-06 22:08:14 +0900

    1-4修正

--- JavaScript/1-4/main.js
+++ JavaScript/1-4/main.js
@@ -1,15 +1,11 @@
-let number = 1;
-while (number <= 100) {
-    if (number % 15 === 0) {
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
         console.log("FizzBuzz!!");
+    } else if (i % 3 === 0) {
+        console.log("Fizz!");
+    } else if (i % 5 === 0) {
+        console.log("Buzz!");
+    } else {
+        console.log(i);
     }
-    else if (number % 3 === 0) {
-        console.log("Fizz!!");
-    }
-    else if (number % 5 === 0) {
-        console.log("Buzz!!");
-    }else{
-        console.log(number);
-    }
-    number++;
 }
