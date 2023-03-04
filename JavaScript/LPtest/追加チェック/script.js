dff1723496776968f4df1a4be0e6c984362a7d7c
kouhei-r-g <103445080+kouhei-r-g@users.noreply.github.com>
2023-03-04 22:06:10 +0900

    LPTEST

---
+++ JavaScript/LPtest/追加チェック/script.js
@@ -0,0 +1,14 @@
+const form1btn = document.querySelector('#form1btn');
+const formcontainer1 = document.querySelector('#formcontainer1');
+
+form1btn.addEventListener('click', () => {
+    formcontainer1.style.display = 'block';
+});
+
+
+const form2btn = document.querySelector('#form2btn');
+const formcontainer2 = document.querySelector('#formcontainer2');
+
+form2btn.addEventListener('click', () => {
+    formcontainer2.style.display = 'block';
+});
